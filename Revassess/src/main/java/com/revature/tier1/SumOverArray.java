8c2a2eedd57df19cf3fab7ccf96de4160fe8d109
Ryan Gerold <ryan.gerold@gmail.com>
2020-07-01 12:05:54 -0400

    did tier 1

--- Revassess/src/main/java/com/revature/tier1/SumOverArray.java
+++ Revassess/src/main/java/com/revature/tier1/SumOverArray.java
@@ -3,6 +3,15 @@ package com.revature.tier1;
 public class SumOverArray {
 
 	public static int IterateAndSum(int[] arr) {
-		return 0;
+		if (arr == null) {
+			return 0;
+		}
+		
+		Integer total = 0;
+		
+		for (int i=0; i<arr.length; i++) {
+			total += arr[i];
+		}
+		return total;
 	}
 }
